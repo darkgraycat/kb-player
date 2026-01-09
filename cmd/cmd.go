46c205edf59cbf02a7d57de3823335ec2a11db74
darkgraycat <jburyanov@gmail.com>
2026-01-09 21:52:46 +0200

    : some experimentation with channels

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,18 +4,38 @@ import (
 	"kbplayer/internal/audio"
 	"kbplayer/internal/tui"
 	"os"
+	"time"
+)
+
+type Command int
+
+const (
+	CmdStart Command = iota
+	CmdStop
+	CmdExit
 )
 
 func Execute(cfg *Config) error {
-	// TODO: next setup synth
-
 	var output audio.Output
 	if cfg.Output.Mode == "stream" {
-		output = audio.NewStreamOutput(cfg.Output.Command, cfg.Output.Args...)
+		// output = audio.NewStreamOutput(cfg.Output.Command, cfg.Output.Args...)
+		panic("Stream mode is not implemented yet")
 	} else {
 		output = audio.NewFileOutput(cfg.Output.Command, cfg.Output.Args...)
 	}
 
+	wavChan := make(chan *audio.WAV, 10)
+	ctlChan := make(chan Command, 1)
+
+	go audioLoop(output, wavChan, ctlChan)
+
+	rawModeLoop(cfg, wavChan, ctlChan)
+
+	tui.ClearScreen()
+	return nil
+}
+
+func rawModeLoop(cfg *Config, wavChan chan *audio.WAV, ctlChan chan Command) {
 	wavMap := setupWavMap(cfg)
 
 	tui.WithRaw(int(os.Stdin.Fd()), func() (any, error) {
@@ -28,18 +48,36 @@ func Execute(cfg *Config) error {
 			ch := buf[0]
 			switch ch {
 			case byte(cfg.Keymap.Quit):
+				ctlChan <- CmdStop
 				return nil, nil
 			}
 
 			if wav, ok := wavMap[ch]; ok {
-				go output.Play(wav)
+				wavChan <- wav
 			}
-
 		}
 	})
+}
 
-	tui.ClearScreen()
-	return nil
+func audioLoop(output audio.Output, wavChan chan *audio.WAV, ctlChan chan Command) {
+	// TODO: simulate streaming using ticker
+	// or even move it into new audio.Output implementation
+	ticker := time.NewTicker(60 * time.Millisecond)
+	defer ticker.Stop()
+
+	running := true
+	for running {
+		select {
+		case wav := <-wavChan:
+			go output.Play(wav)
+		case <-ticker.C:
+		case cmd := <-ctlChan:
+			switch cmd {
+			case CmdStop:
+				running = false
+			}
+		}
+	}
 }
 
 func setupWavMap(cfg *Config) map[byte]*audio.WAV {
